d7001001a329e1b65b640468654b6e61cbeb40ec
Miranda Melina <mmiranda@yopapp.me>
2020-01-28 16:29:12 -0300

    first commit

---
+++ CollectionView/CollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionView
+//
+//  Created by Melina Miranda on 28/01/2020.
+//  Copyright © 2020 Melina Miranda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
